71d893e6eb29e2acb8161508b908906d494fd7be
lotfizs <2133349@talnet.nl>
2021-09-08 12:15:07 +0200

    update

--- php/login.php
+++ php/login.php
@@ -46,41 +46,80 @@ include 'connect.php';
 ?>
 
  <!DOCTYPE html>
- <html lang="en" xmlns="http://www.w3.org/1999/html">
+ <html lang="en">
  <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <title>Footgolf</title>
-
-
-     <link rel="stylesheet" href="style.css">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-
+     <meta charset="UTF-8" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     <script
+             src="https://kit.fontawesome.com/64d58efce2.js"
+             crossorigin="anonymous"
+     ></script>
+     <link rel="stylesheet" href="style.css" />
+     <title>Sign in & Sign up Form</title>
  </head>
  <body>
-
- <div class="wrapper">
-     <section class="form login">
-         <header>Login</header>
-         <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
-             <div class="error-text"></div>
-             <div class="name-details">
-                 <div class="field input">
-                     <label>Spelernaam</label>
-                     <input type="text" name="speler-naam" placeholder="Enter your game name" required>
+ <div class="container">
+     <div class="forms-container">
+         <div class="signin-signup">
+             <form action="#" class="sign-in-form">
+                 <h2 class="title">Sign in</h2>
+                 <div class="input-field">
+                     <i class="fas fa-user"></i>
+                     <input type="text" placeholder="Username" />
                  </div>
-                 <div class="field input">
-                     <label>Email</label>
-                     <input type="text" name="speler-email" placeholder="Enter your email" required>
+                 <div class="input-field">
+                     <i class="fas fa-phone"></i>
+                     <input type="number" placeholder="Telefoon nummer" />
                  </div>
-             </div>
-             <div class="field button">
-                 <input type="submit" name="submit" value="Continue to Play">
-             </div>
-         </form>
-     </section>
+             </form>
+             <form action="#" class="sign-up-form">
+                 <h2 class="title">Sign up</h2>
+                 <div class="input-field">
+                     <i class="fas fa-user"></i>
+                     <input type="text" placeholder="Username" />
+                 </div>
+                 <div class="input-field">
+                     <i class="fas fa-envelope"></i>
+                     <input type="email" placeholder="Email" />
+                 </div>
+                 <div class="input-field">
+                     <i class="fas fa-phone"></i>
+                     <input type="number" placeholder="Telefoon nummer" />
+                 </div>
+                 <input type="submit" class="btn" value="Registreer" />
+             </form>
+         </div>
+     </div>
 
+     <div class="panels-container">
+         <div class="panel left-panel">
+             <div class="content">
+                 <h3>Nieuw hier?</h3>
+                 <p>
+                     Registreer nu om de ultieme Footgolf everaring te belevening?
+                 </p>
+                 <button class="btn transparent" id="sign-up-btn">
+                     Registreer
+                 </button>
+                 <img src="img/Footgolf-logo.png" class="image" alt="" />
+             </div>
+         </div>
 
+         <div class="panel right-panel">
+             <div class="content">
+                 <h3>Een van ons?</h3>
+                 <p>
+                     Log in, om weer te leukste ervaring te beleven
+                 </p>
+                 <button class="btn transparent" id="sign-in-btn">
+                     Log in
+                 </button>
+                 <img src="img/Footgolf-logo.png" class="image2" alt="">
+             </div>
+         </div>
+     </div>
  </div>
+
+ <script src="../Javascript/app.js"></script>
  </body>
+ </html>
