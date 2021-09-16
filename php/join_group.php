e423013e05839490965385357e7f91990286db09
lotfizs <2133349@talnet.nl>
2021-09-16 15:01:39 +0200

    update

--- php/join_group.php
+++ php/join_group.php
@@ -1,60 +1,48 @@
-<?php
-include_once 'connect.php';
-    echo "<form action='#' method='post'>
-     <label>Wat is het groep nummer?</label>
-        <input type='text' name='GroepID' id='GroepID'>
-        <input type='submit'>
-    </form>";
-$user_input=isset($_POST['GroepID']);
-$res=mysqli_query($conn, "select groupID, `Speler_aantal` from groep where groupID=$user_input");
-//Check of query heeft gerunt
-if($res) {
-    //loop door de $row resultaten heen en doe het in $row
-    while ($row = mysqli_fetch_assoc($res)) {
-        $player_aantal=$row['Speler_aantal'];
-    }
-    //Als speler aantal groter of gelijk is aan 5 dan geeft hij die error
-    if (isset($player_aantal)>=5){
-        echo "Sorry deze groep zit vol";
-    } else{
-        //verhoog player aantal
-        mysqli_query($conn, "update groep set `Speler_aantal`= Speler_aantal+1 where groupID=$user_input");
-        //Zet groepID
-        $_SESSION['groupID']=$user_input;
-        print_r($_SESSION);
-        //Geef aantal spelers weer
-        echo "<h2 id='pc'> </h2>";
-    }
-} else{
-    echo "Deze groep staat niet in onze database, probeer aub opnieuw";
-}
-?>
-//dfskhskshghsdkslkjsdghjhkHkghfglslgkdghdkghshjlkg lgguyoisroigurkajsdhfhdsk fdahdLK";lkjfdh;JhJkLkhklsfskf hdsfkghks
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Footgolf - Score</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="stylegroep.css" />
+    <title>Footgolf - Log in / Sign up</title>
 </head>
+
 <body>
+<div class="container">
+    <div class="forms-container">
+        <!--Sign in form -->
+        <div class="signin-signup">
+            <form action="join_group_backend.php" class="sign-in-form" method="post">
+                <h2 class="title">Voer Game Code </h2>
+
+                <?php
+                include 'connect.php';
+                $errors=$_SESSION['Errors'];
+                if (isset($_SESSION['Errors'])) {echo "<div class='error-text'>".$errors."</div>";}
+                $errors=' ';
+                unset($_SESSION['Errors'])
+                ?>
+<div class="input-field">
+    <i class="fas fa-lock"></i>
+    <input type="text" placeholder="GameCode" name="GroepID"/>
+</div>
+<input type="submit" class="btn" value="Log in" />
+                <div class="panel right-panel">
+                    <div class="content">
+                        <h3>Een van ons?</h3>
+                        <p>
+                            Log in, om weer de leukste ervaring te beleven!
+                        </p>
+                        <button class="btn transparent" id="sign-in-btn">
+                            Log in
+                        </button>
+
+<script src="../Javascript/app.js"></script>
+
+
 </body>
 <script src='../Javascript/jquery.min.js'></script>
-<script>
-    //maak functie aan.
-    function update_var() {
-        jQuery.ajax({
-            url: 'pc_fetch.php', //Script URL.
-            method: 'POST', //Methode data doorgeven
-            success: function (answer) {
-                jQuery('#pc').html(answer);//update your div with new content ....
-            },
-            error: function () {
-                //Iets ging fout, niet sure wat tho
-                alert('shit brokey');
-            }
-        });
-    }
-    //Call de functie elke ~2.7 sec
-    setInterval(function(){ update_var() }, 27000);
-</script>
 </html>
---
+++ php/join_group_backend.php
@@ -0,0 +1,32 @@
+<?php
+include_once 'connect.php';
+$user_input=isset($_POST['GroepID']);
+//Check of query heeft gerunt
+if(!empty($user_input)) {
+    $res=mysqli_query($conn, "select groupID, `Speler_aantal` from groep where groupID=$user_input");
+    if ($res) {
+        //loop door de $row resultaten heen en doe het in $row
+        while ($row = mysqli_fetch_assoc($res)) {
+            $player_aantal = $row['Speler_aantal'];
+        }
+        //Als speler aantal groter of gelijk is aan 5 dan geeft hij die error
+        if (isset($player_aantal) >= 5) {
+            $_SESSION['Errors'] = "Sorry deze groep zit vol";
+        } else {
+            //verhoog player aantal
+            mysqli_query($conn, "update groep set `Speler_aantal`= Speler_aantal+1 where groupID=$user_input");
+            //Zet groepID
+            $_SESSION['groupID'] = $user_input;
+            header("location:join_group.php");
+            //Geef aantal spelers weer
+            //echo "<h2 id='pc'> </h2>";
+        }
+    } else {
+        $_SESSION['Errors'] = "Deze groep staat niet in onze database, probeer aub opnieuw";
+        header("location:join_group.php");
+    }
+}
+else{
+    echo "fail";
+    //header("location:join_group.php");
+}
