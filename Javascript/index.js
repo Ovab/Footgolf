ab062dbdd6412554f66f1514f3d7c5ba93916512
Niels Kloosterman <70892664+Niels1325@users.noreply.github.com>
2021-09-20 10:43:30 +0200

    Meer javascript toegevoegt

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,18 +1,51 @@
 const spelRegels = document.getElementById('spelregels');
+const gholes = document.getElementById('gholes');
+const reservation = document.getElementById('reservation');
 const title = document.getElementById('title');
 
 //Voor als je het standaart weg wilt hebben.
 //spelRegels.style.display = "none";
 //title.style.display = "none";
+gholes.style.display = "none";
+reservation.style.display = "none";
 
 function toggleSpelregels() {
-    if (spelRegels.style.display === "none") {
-    title.innerHTML = "Spelregels";
-    title.style.display = "block";
-    spelRegels.style.display = "flex";
+    if (spelRegels.style.display === "none" && spelRegels.style.display === "none" && gholes.style.display === "none") {
+        title.innerHTML = "Spelregels";
+        title.style.display = "block";
+        //gholes.style.display = "none";
+        //reservation.style.display = "none";
+        spelRegels.style.display = "flex";
     } else {
-        title.innerHTML = "";
         title.style.display = "none";
         spelRegels.style.display = "none";
     }
 }
+
+//function toggleGholes() {
+//    if (gholes.style.display === "none") {
+//        title.innerHTML = "Gholes";
+//        title.style.display = "block";
+//        gholes.style.display = "flex";
+//    } else if (spelRegels.style.display === "flex" && gholes.style.display === "flex") {
+//        spelRegels.style.display = "none";
+//        reservation.style.display = "none";
+//    } else {
+//        title.style.display = "none";
+//        gholes.style.display = "none";
+//   }
+//}
+
+//function toggleReservation() {
+//    if (reservation.style.display === "none") {
+//        title.innerHTML = "Reservation";
+//        title.style.display = "block";
+ //       reservation.style.display = "flex";
+ //   } else if (spelRegels.style.display === "flex" && gholes.style.display === "flex") {
+ //       spelRegels.style.display = "none";
+ //       gholes.style.display = "none";
+  //  } else {
+ //       title.style.display = "none";
+ //       reservation.style.display = "none";
+//    }
+//}
