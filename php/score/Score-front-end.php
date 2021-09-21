65b38b7f9dcd92efa1056341027dbcbf68520fe0
lotfizs <2133349@talnet.nl>
2021-09-21 09:32:54 +0200

    update

--- php/score/Score-front-end.php
+++ php/score/Score-front-end.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Footgolf - Score</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <?php include('../connect.php');print_r($_SESSION);
