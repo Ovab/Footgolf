8c4af8decac5c4a9f4969f8ccdec37a02cdb0512
lotfizs <2133349@talnet.nl>
2021-09-22 13:40:24 +0200

    update

--- Javascript/index.js
+++ Javascript/index.js
@@ -22,32 +22,20 @@ function toggleSpelregels() {
     }
 }
 
-function toggleGholes() {
-    if (gholes.style.display === "none" && gholes.style.display === "none" && gholes.style.display === "none") {
-        title.innerHTML = "Gholes";
-        title.style.display = "block";
-        //gholes.style.display = "none";
-        //reservation.style.display = "none";
-        spelRegels.style.display = "flex";
-    } else {
-        title.style.display = "none";
-        spelRegels.style.display = "none";
-    }
-}
 
-//function toggleGholes() {
-//    if (gholes.style.display === "none") {
-//        title.innerHTML = "Gholes";
-//        title.style.display = "block";
-//        gholes.style.display = "flex";
-//    } else if (spelRegels.style.display === "flex" && gholes.style.display === "flex") {
-//        spelRegels.style.display = "none";
-//        reservation.style.display = "none";
-//    } else {
-//        title.style.display = "none";
-//        gholes.style.display = "none";
-//   }
-//}
+function toggleGholes() {
+   if (gholes.style.display === "none") {
+        title.innerHTML = "Gholes";
+       title.style.display = "block";
+     gholes.style.display = "flex";
+   } else if (spelRegels.style.display === "flex" && gholes.style.display === "flex") {
+       spelRegels.style.display = "none";
+       reservation.style.display = "none";
+    } else {
+       title.style.display = "none";
+       gholes.style.display = "none";
+   }
+}
 
 //function toggleReservation() {
 //    if (reservation.style.display === "none") {
