1fb8f6d4ca5c52342acc578cbd0478c77ee41d90
Niels Kloosterman <70892664+Niels1325@users.noreply.github.com>
2021-10-13 09:57:28 +0200

    Vraag Scores Front End

--- php/Funcs/vraag-scores.php
+++ php/Funcs/vraag-scores.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../stylegroep.css"/>
+    <link rel="stylesheet" href="vraagscores.css"/>
     <title>Footgolf - Vraag scores</title>
 </head>
 
 <body>
-<div class="container">
+<div>
     <div class="forms-container">
         <!--Sign in form -->
         <div class="signin-signup">
@@ -47,42 +47,60 @@
                              <th colspan='100%'>Groep-naam: $groepnaam </th><tr></tr>
                             <th>Hole:</th>
                             <th>&nbsp$naam1&nbsp</th>";
-                    if(isset($naam2)){echo "<th>&nbsp$naam2&nbsp</th>";}
-                    if(isset($naam3)){echo "<th>&nbsp$naam3&nbsp</th>";}
-                    if(isset($naam4)){echo "<th>&nbsp$naam4&nbsp</th>";}
-                           echo "<tr>";
+                    if (isset($naam2)) {
+                        echo "<th>&nbsp$naam2&nbsp</th>";
+                    }
+                    if (isset($naam3)) {
+                        echo "<th>&nbsp$naam3&nbsp</th>";
+                    }
+                    if (isset($naam4)) {
+                        echo "<th>&nbsp$naam4&nbsp</th>";
+                    }
+                    echo "<tr>";
                     while ($row = mysqli_fetch_assoc($spellen)) {
-                        $speler2=$row['Speler2'];
-                        $speler3=$row['Speler3'];
-                        $speler4=$row['Speler4'];
+                        $speler2 = $row['Speler2'];
+                        $speler3 = $row['Speler3'];
+                        $speler4 = $row['Speler4'];
                         echo '
 							<td>' . $row['Hole'] . '</td>
 							<td>' . $row['Speler1'] . '</td>
 							';
-                        if(isset($speler2)) {echo "<td> $speler2 </td>";}
-                        if(isset($speler3)) {echo "<td> $speler3 </td>";}
-                        if(isset($speler4)) {echo "<td> $speler4 </td>";}
-						  echo "</tr>";
+                        if (isset($speler2)) {
+                            echo "<td> $speler2 </td>";
+                        }
+                        if (isset($speler3)) {
+                            echo "<td> $speler3 </td>";
+                        }
+                        if (isset($speler4)) {
+                            echo "<td> $speler4 </td>";
+                        }
+                        echo "</tr>";
                     }
+                    $_SESSION ['tablepost'] = true;
                 }
                 ?>
                 <div class="input-field">
                     <i class="fas fa-lock"></i>
-                    <input type="text" inputmode="numeric" pattern="[0-9]*" placeholder="GameCode" name="GameCode"/>
+                    <input type="text" inputmode="numeric" pattern="[0-9]*" placeholder="Game Code" name="GameCode"/>
                 </div>
                 <input type="submit" class="btn" value="Vraag scores"/>
+            </form>
+        </div>
+    </div>
+</div>
+<p id="pc"></p>
 
-                <p id="pc"></p>
+<div class="panel right-panel">
+    <div class="content">
+        <h3>Een van ons?</h3>
+        <p>
+            Log in, om weer de leukste ervaring te beleven!
+        </p>
+        <button class="btn transparent" id="sign-in-btn">Join</button>
 
-                <div class="panel right-panel">
-                    <div class="content">
-                        <h3>Een van ons?</h3>
-                        <p>
-                            Log in, om weer de leukste ervaring te beleven!
-                        </p>
-                        <button class="btn transparent" id="sign-in-btn">Join</button>
 
-                        <script src="../../Javascript/app.js"></script>
+    </div>
+</div>
 </body>
 <script src='../../Javascript/jquery.min.js'></script>
 </html>
