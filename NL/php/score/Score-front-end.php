6102218ea33b793dcc5cdae92748ecdd867fcd55
lotfizs <2133349@talnet.nl>
2021-11-29 11:26:34 +0100

    slider maken voor de holes

--- NL/php/score/Score-front-end.php
+++ NL/php/score/Score-front-end.php
@@ -79,8 +79,70 @@
         echo "<a href='../../index.php'> <input class='btn' type='submit' value='Naar Home pagina'> </a> ";
     }
     ?>
-
-
 </div>
+
+<div class="slide-container">
+
+    <div class="wrapper">
+        <div class="clash-card barbarian">
+            <div class="clash-card__image clash-card__image--barbarian">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/barbarian.png" alt="barbarian" />
+            </div>
+            <div class="clash-card__level clash-card__level--barbarian">Level 4</div>
+            <div class="clash-card__unit-name">The Barbarian</div>
+            <div class="clash-card__unit-description">
+                The Barbarian is a kilt-clad Scottish warrior with an angry, battle-ready expression, hungry for destruction. He has Killer yellow horseshoe mustache.
+            </div>
+
+            <div class="clash-card__unit-stats clash-card__unit-stats--barbarian clearfix">
+                <div class="one-third">
+                    <div class="stat">20<sup>S</sup></div>
+                    <div class="stat-value">Training</div>
+                </div>
+
+                <div class="one-third">
+                    <div class="stat">16</div>
+                    <div class="stat-value">Speed</div>
+                </div>
+
+                <div class="one-third no-border">
+                    <div class="stat">150</div>
+                    <div class="stat-value">Cost</div>
+                </div>
+
+            </div>
+
+        </div> <!-- end clash-card barbarian-->
+    </div> <!-- end wrapper -->
+
+    <div class="wrapper">
+        <div class="clash-card archer">
+            <div class="clash-card__image clash-card__image--archer">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/archer.png" alt="archer" />
+            </div>
+            <div class="clash-card__level clash-card__level--archer">Level 5</div>
+            <div class="clash-card__unit-name">The Archer</div>
+            <div class="clash-card__unit-description">
+                The Archer is a female warrior with sharp eyes. She wears a short, light green dress, a hooded cape, a leather belt and an attached small pouch.
+            </div>
+
+            <div class="clash-card__unit-stats clash-card__unit-stats--archer clearfix">
+                <div class="one-third">
+                    <div class="stat">25<sup>S</sup></div>
+                    <div class="stat-value">Training</div>
+                </div>
+
+                <div class="one-third">
+                    <div class="stat">24</div>
+                    <div class="stat-value">Speed</div>
+                </div>
+
+                <div class="one-third no-border">
+                    <div class="stat">300</div>
+                    <div class="stat-value">Cost</div>
+                </div>
+
+            </div>
+            
 </body>
 </html>
