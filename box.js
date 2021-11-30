5a771d820522303f857814ac305af956587915bf
lotfizs <2133349@talnet.nl>
2021-11-30 13:13:16 +0100

    score invoeren

--- box.js
+++ box.js
@@ -1,89 +1,49 @@
-$(document).ready(function() {
-    var getProductHeight = $('.product.active').height();
+// Cards
+let card1 = document.getElementById("card1"),
+    card2 = document.getElementById("card2"),
+    card3 = document.getElementById("card3"),
+    card4 = document.getElementById("card4"),
+    card_length = document.querySelectorAll(".card");
 
-    $('.products').css({
-        height: getProductHeight
-    });
+// container
+let container = document.getElementById("container");
 
-    function calcProductHeight() {
-        getProductHeight = $('.product.active').height();
+// Card List
+let card_list = [card1, card2, card3, card4];
 
-        $('.products').css({
-            height: getProductHeight
-        });
+// next function
+let counter = 1;
+function next() {
+    console.log("next:" + counter);
+    if (counter < card_length.length) {
+        card_list[counter].classList.add("active");
+        backgroundFunc(counter);
+        counter += 1;
+    } else {
+        counter = card_length.length;
     }
+}
 
-    function animateContentColor() {
-        var getProductColor = $('.product.active').attr('product-color');
-
-        $('body').css({
-            background: getProductColor
-        });
-
-        $('.title').css({
-            color: getProductColor
-        });
-
-        $('.btn').css({
-            color: getProductColor
-        });
+function prev() {
+    if (counter > 1) {
+        card_list[counter - 1].classList.remove("active");
+        counter -= 1;
+    } else {
+        counter = 1;
     }
+    backgroundFunc(counter - 1);
+    console.log("prev:" + counter);
+}
 
-    var productItem = $('.product'),
-        productCurrentItem = productItem.filter('.active');
-
-    $('#next').on('click', function(e) {
-        e.preventDefault();
-
-        var nextItem = productCurrentItem.next();
-
-        productCurrentItem.removeClass('active');
-
-        if (nextItem.length) {
-
-            productCurrentItem = nextItem.addClass('active');
-        } else {
-            productCurrentItem = productItem.first().addClass('active');
-        }
-
-        calcProductHeight();
-        animateContentColor();
-    });
-
-    $('#prev').on('click', function(e) {
-        e.preventDefault();
-
-        var prevItem = productCurrentItem.prev();
-
-        productCurrentItem.removeClass('active');
-
-        if (prevItem.length) {
-            productCurrentItem = prevItem.addClass('active');
-        } else {
-            productCurrentItem = productItem.last().addClass('active');
-        }
-
-        calcProductHeight();
-        animateContentColor();
-    });
-
-    // Ripple
-    $('[ripple]').on('click', function(e) {
-        var rippleDiv = $('<div class="ripple" />'),
-            rippleSize = 60,
-            rippleOffset = $(this).offset(),
-            rippleY = e.pageY - rippleOffset.top,
-            rippleX = e.pageX - rippleOffset.left,
-            ripple = $('.ripple');
-
-        rippleDiv.css({
-            top: rippleY - (rippleSize / 2),
-            left: rippleX - (rippleSize / 2),
-            background: $(this).attr("ripple-color")
-        }).appendTo($(this));
-
-        window.setTimeout(function() {
-            rippleDiv.remove();
-        }, 1900);
-    });
-});
+// background function
+function backgroundFunc(x) {
+    if (x == 0) {
+        container.style.background = "#de732d";
+    } else if (x == 1) {
+        container.style.background = "#7c4019";
+    } else if (x == 2) {
+        container.style.background = "#8b8b8b";
+    } else if (x == 3) {
+        container.style.background = "#e8672e";
+    }
+}
