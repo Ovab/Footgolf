04acf892cf1d6bb517c2cbb7d5a30b57356f3eba
lotfizs <2133349@talnet.nl>
2021-11-30 13:30:32 +0100

    score invoeren

--- box.js
+++ box.js
@@ -3,6 +3,7 @@ let card1 = document.getElementById("card1"),
     card2 = document.getElementById("card2"),
     card3 = document.getElementById("card3"),
     card4 = document.getElementById("card4"),
+    card5 = document.getElementById("card5"),
     card_length = document.querySelectorAll(".card");
 
 // container
