b418625c794a15ab45392129f37e40a1b33beea1
Niels Kloosterman <70892664+Niels1325@users.noreply.github.com>
2021-12-07 13:49:54 +0100

    update

--- EN/php/Groep/join_group.php
+++ EN/php/Groep/join_group.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../stylegroep.css"/>
-    <title>Footgolf - Group Join</title>
+    <title>Footgolf - Join Group</title>
 </head>
 
 <body>
